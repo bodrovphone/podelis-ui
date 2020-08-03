1e78a3e0285cdebab3750fa30a11de8fbe4e3a63
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-08-03 17:33:20 +0300

    serverless testing

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  target: 'serverless', // <- add here
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
+  target: 'serverless'
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Main Page Zadelis</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -14,7 +16,9 @@ export default function Home() {
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          <Link href="/test">
+            <a>Test Page</a>
+          </Link>
         </p>
 
         <div className="grid">
