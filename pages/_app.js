15e438f4420ecf5a4b99a9ea0e49a6337e9881db
Oleksandr Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-08-18 00:06:12 +0300

    sideBar context

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from '../components/styles/theme';
+import { SideBarProvider } from '../components/context/sideBar.context';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <SideBarProvider>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </SideBarProvider>
     )
   }
 }
