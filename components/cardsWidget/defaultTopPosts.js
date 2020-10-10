233b1d1ec24d52d943ee59938013e1112ba864d0
Alex Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-10-11 00:27:19 +0300

    defaultTopPosts + fetch posts

---
+++ components/cardsWidget/defaultTopPosts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SingleCard from '../singleCard';
+
+const defaultTopPosts = [
+  <SingleCard title="Проектор Xiomi" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[0]'} />,
+
+  <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]'} />,
+
+  <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwj iejf wfjw р Xiomi sdfsdf pwj efpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[2]'} />
+];
+
+export default defaultTopPosts;
--- components/cardsWidget/index.js
+++ components/cardsWidget/index.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CardsStyled } from './styles';
 import SingleCard from '../singleCard';
+import defaultTopPosts from './defaultTopPosts';
 
 const CardsWidget = (props) => {
+  const [ posts, setPosts ] = useState(defaultTopPosts);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(json => {
+      let postsFromApi = [];
+      json.map(post => {
+        if (post.id > 90) {
+          postsFromApi.push(
+            <SingleCard title={post.title} description={post.body} imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id={String(post.id)} key={post.id} />
+          );
+        }
+      });
+      console.log('posts fetched, updating...');
+      setPosts([...posts, ...postsFromApi]);
+    }).catch(error => console.log(error));
+  }, [])
   return (
     <CardsStyled>
-          <SingleCard title="Проектор Xiomi" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[0]'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwj iejf wfjw р Xiomi sdfsdf pwj efpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[2]'} />
-
-          <SingleCard title="Проектор Xiomi" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[0]asds'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]23r'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwj iejf wfjw р Xiomi sdfsdf pwj efpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[2]fer'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]23r'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwj iejf wfjw р Xiomi sdfsdf pwj efpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[2]fer'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]23r'} />
-
-          <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwj iejf wfjw р Xiomi sdfsdf pwj efpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество..." imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[2]fer'} />
-
+      {
+        posts.map(item => item)
+      }
     </CardsStyled>
   );
 }
