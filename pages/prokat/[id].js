01dc153a4d5146deecffa66a8e594da504f8c56e
Alex Bodrov <oleksandr.bodrov@bazaarvoice.com>
2020-10-13 22:37:34 +0300

    added prokat route with a dummy template

---
+++ pages/prokat/[id].js
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+
+import Layout, { Main, H1, Description, Centered } from '../../components/layouts';
+import SingleCard from '../../components/singleCard';
+import Footer from '../../components/footer';
+
+const Prokat = (props) => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log( id );
+  return (
+    <Layout>
+      <Main>
+        <H1>
+          <div>
+            Zadelis
+          </div>
+          <span>На прокат бери - деньги береги.</span>
+          {/*укр: В оренду забирай - грошi зберiгай. */}
+        </H1>
+
+        <SingleCard title="Проектор Xiomi sdfsdfpwj efpwiejf wfjw р Xiomi sdfsdf pwjefpw iejf w" description="Прекрасный проектор для просмотра фильмов с друзъями. Предлогаем разные модели. Простой в управлении. 4k качество ктор для просмотра фильмов с друзъями. Предлогактор для просмотра фильмов с друзъями. Предлога" imageSrc="https://images.ua.prom.st/2139978779_w640_h640_proektor-xiaomi-mi.jpg" id="fdsfks9werksdf" key={'props.posts[1]'} />
+        
+      </Main>
+
+      <Footer />
+    </Layout>
+  );
+}
+
+export default Prokat;
