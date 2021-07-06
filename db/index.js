91cb040e17624bc2cad984ed004e1a2c0de23590
Alex Bodrov <oleksandr.bodrov@bazaarvoice.com>
2021-07-06 14:04:19 +0300

    post-prokat api - simple post with no validation

--- db/index.js
+++ db/index.js
@@ -1,11 +1,17 @@
-const MongoClient = require("mongodb").MongoClient;
+"use strict";
+
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGO_DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const clientPromise = client.connect();
 
 // wrap this shit into wrapper to be able to make calls to API and don't make connection?
-export default async function getData(collection, query = {}) {
-  const client = await MongoClient.connect(process.env.MONGO_DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+export async function getData(collection, query = {}) {
+  let client = await clientPromise;
   let db = client.db("zadelis");
 
   let data = await db
@@ -17,4 +23,13 @@ export default async function getData(collection, query = {}) {
   client.close();
 
   return data;
+}
+
+export async function postProkat(payload) {
+  let client = await clientPromise;
+  let db = client.db("zadelis");
+
+  let response = await db.collection("prokats").insertOne(payload);
+
+  return response.insertedCount === 1 && response.insertedId;
 }
---
+++ pages/api/post-prokat.js
@@ -0,0 +1,26 @@
+import { postProkat } from "../../db";
+
+function validatePostInput(payload) {
+  // https://dev.to/meddlesome/nextjs-apis-validator-with-middleware-3njl
+  // тут тоже самое вроде как - https://nextjs.org/docs/api-routes/api-middlewares
+}
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    // 1. validate payload
+    if (req.body.payload)
+      // 2. check user
+      // 3. post to mongo
+      // 4. response with the satus (done)
+      try {
+        const response = await postProkat(req.body.payload);
+        res.status(200).json(response);
+      } catch (error) {
+        res
+          .status(501)
+          .json({ message: "was not unable to create post on DB", error });
+      }
+  } else {
+    res.status(401).json({ message: "wrong request type, check method" });
+  }
+}
